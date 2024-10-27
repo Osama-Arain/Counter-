6f3d82ba2cc05a1d46bc73ce737e525c9800f523
Osama-Arain <osaamaarain@gmail.com>
2024-10-28 00:15:10 +0500

    Counter Commit

---
+++ App.js
@@ -0,0 +1,21 @@
+let counter = 0;
+let count =document.querySelector("#Count");
+
+function Re(){
+    counter=0;
+    count.innerHTML=counter;
+
+
+}
+
+function Dc(){
+    counter--
+    count.innerHTML=counter;
+
+}
+
+function In (){
+    counter++
+    count.innerHTML=counter;
+}
+
